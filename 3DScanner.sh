811e896d83dd53558eebd729ec2a47ee20796476
Marco Bellan <mark.sg97@gmail.com>
2016-03-23 18:37:54 +0100

    Update 3DScanner.sh

--- 3DScanner.sh
+++ 3DScanner.sh
@@ -1,6 +1,15 @@
 #! /bin/sh
 # /etc/init.d/example
- 
+### BEGIN INIT INFO
+# Provides:          3DScanner
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: WebUI for 3DScanner.
+# Description:       Enable WebUI for 3D Scanning.
+### END INIT INFO
+
 case "$1" in
   start)
     sudo python /home/pi/3DScanner/Site.py &
@@ -15,5 +24,5 @@ case "$1" in
     exit 1
     ;;
 esac
- 
+
 exit 0
